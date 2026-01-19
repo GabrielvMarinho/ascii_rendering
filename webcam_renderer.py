f3bba6bc0ac688be541001dd0dbcac5035048880
marinho <gabrielvmarinho1711@gmail.com>
2026-01-19 19:47:02 -0300

    Add first working version update webcam renderer

--- webcam_renderer.py
+++ webcam_renderer.py
@@ -1,7 +1,7 @@
 import cv2
+import os
 
-
-darkness_scale = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]         
+darkness_scale = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]    
 
 vc = cv2.VideoCapture(0)
 
@@ -13,22 +13,24 @@ else:
 while result := vc.read():
 
 	rval, frame = result
-	print(frame.shape)
+	
 	width, height, _ = frame.shape	
 
 	
 	ascii_characters = ""                                                       
                                                                                  
-	for y in range(height):                                                     
-		for x in range(width):                                                   
-			r, g, b = frame[x, y]                                                                                              
-			darkness = (r+g+b)/(255*3)                                            
-			                                                   
-			darkness_index = int(darkness*9)                           
-			ascii_characters += darkness_scale[darkness_index]
-		ascii_characters += "\n"
 	
+	for y in range(width):                                                     
+		if y%5==0:
+			for x in range(height):                                                   
+				if x%2==0:
+					r, g, b = frame[y, x]                                                                                              
+					darkness = (r+g+b)/(255*3)					                                            			                                                   
+					darkness_index = int(darkness*9)                           
+					ascii_characters += darkness_scale[darkness_index]
+			ascii_characters += "\n"
+	
+	os.system('clear')
 	print(ascii_characters)
-		
 
 vc.release()
